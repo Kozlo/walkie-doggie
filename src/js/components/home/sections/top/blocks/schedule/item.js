4c144e5545d1e0622c2b22436d7ad55e895121ca
Martins Kozlovskis <mkozlovskis@gmail.com>
2016-05-16 09:46:04 +0200

    Modified schedule code by splitting up sessions into a separate module

--- src/js/components/home/sections/top/blocks/schedule/item.js
+++ src/js/components/home/sections/top/blocks/schedule/item.js
@@ -1,33 +1,30 @@
 import React from "react";
 
+// import components
+import ItemSession from "./session";
+
 export default class ScheduleItem extends React.Component {
     render() {
+        const oSession1 = {
+            title: "Early Exercise",
+            time: "8:30 - 10:00"
+        };
+        const oSession2 = {
+            title: "Muscle Building",
+            time: "8:30 - 10:00"
+        };
+        const oSession3 = {
+            title: "Cardio",
+            time: "8:30 - 10:00"
+        };
+
         return (
             <div className="item">
-                <div className="schedule-row row">
-                    <div className="col-xs-6">
-                        <h5 className="regular white">Early Exercise</h5>
-                    </div>
-                    <div className="col-xs-6 text-right">
-                        <h5 className="white">8:30 - 10:00</h5>
-                    </div>
-                </div>
-                <div className="schedule-row row">
-                    <div className="col-xs-6">
-                        <h5 className="regular white">Muscle Building</h5>
-                    </div>
-                    <div className="col-xs-6 text-right">
-                        <h5 className="white">8:30 - 10:00</h5>
-                    </div>
-                </div>
-                <div className="schedule-row row">
-                    <div className="col-xs-6">
-                        <h5 className="regular white">Cardio</h5>
-                    </div>
-                    <div className="col-xs-6 text-right">
-                        <h5 className="white">8:30 - 10:00</h5>
-                    </div>
-                </div>
+                <ItemSession time={oSession1.title} title={oSession1.time} />
+
+                <ItemSession time={oSession2.title} title={oSession2.time} />
+
+                <ItemSession time={oSession3.title} title={oSession3.time} />
             </div>
         );
     }
---
+++ src/js/components/home/sections/top/blocks/schedule/session.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class ItemSession extends React.Component {
+    render() {
+        const { title, time } = this.props;
+        
+        return (
+            <div className="schedule-row row">
+                <div className="col-xs-6">
+                    <h5 className="regular white">{title}</h5>
+                </div>
+                <div className="col-xs-6 text-right">
+                    <h5 className="white">{time}</h5>
+                </div>
+            </div>
+        );
+    }
+}
