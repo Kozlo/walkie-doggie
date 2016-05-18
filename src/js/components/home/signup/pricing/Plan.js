d036ac1992f6eabca3b1276d7d5a46c5bc5857c5
Martins Kozlovskis <mkozlovskis@gmail.com>
2016-05-18 23:03:33 +0200

    Split up pricing code into modules

--- src/js/components/home/signup/pricing/Plan.js
+++ src/js/components/home/signup/pricing/Plan.js
@@ -4,44 +4,42 @@ import React from "react";
 import PricingOption from "./Option"
 
 export default class PricingPlan extends React.Component {
+    renderPricingOption(opt, index) {
+        return(
+            <PricingOption
+                key={index}
+                description={opt.description}
+                price={opt.price}
+                url={opt.url} />
+        )
+    }
+
     render() {
-        const oOpt1 = {
+        const aOpts = [{
             description: "1 month membership",
             price: "150$",
             url: "#"
-        };
-
-        const oOpt2 = {
+        }, {
             description: "3 month membership",
             price: "350$",
             url: "#"
-        };
-
-        const oOpt3 = {
+        }, {
             description: "1 year membership",
             price: "1000$",
             url: "#"
-        };
-
-        const oOpt4 = {
+        }, {
             description: "Free trial class",
             price: null,
             url: "#"
-        };
-        
+        }];
+
         return (
             <div className="dropdown">
                 <button id="dLabel" className="form-control form-white dropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Pricing Plan
                 </button>
                 <ul className="dropdown-menu animated fadeIn" role="menu" aria-labelledby="dLabel">
-                    <PricingOption description={oOpt1.description} price={oOpt1.price} url={oOpt1.url} />
-
-                    <PricingOption description={oOpt2.description} price={oOpt2.price} url={oOpt2.url} />
-
-                    <PricingOption description={oOpt3.description} price={oOpt3.price} url={oOpt3.url} />
-
-                    <PricingOption description={oOpt4.description} price={oOpt4.price} url={oOpt4.url} />
+                    {aOpts.map(this.renderPricingOption.bind(this))}
                 </ul>
             </div>
         );
