aee10fe7c05d3a675016c0e923867ef193d043ae
Martins Kozlovskis <mkozlovskis@gmail.com>
2016-05-20 09:18:45 +0200

    Removed unnecessary files and re-arranged some existing ones

--- src/js/client.js
+++ src/js/client.js
@@ -1,14 +1,21 @@
-// import css files from css folder
-import "../../libs/css/animate.css";
-import "../../libs/css/bootstrap.css";
-import "../../libs/css/cardio.css";
-import "../../libs/css/et-icons.css";
-import "../../libs/css/normalize.css";
-import "../../libs/css/owl.css";
+// import favicons
+import "../../libs/img/favicons/apple-touch-icon-60x60.png";
+import "../../libs/img/favicons/apple-touch-icon-57x57.png";
+import "../../libs/img/favicons/favicon-32x32.png";
+import "../../libs/img/favicons/favicon-16x16.png";
 
-// import css files from fonts folder
-import "../../libs/fonts/eleganticons/et-icons.css";
+// import other files
+import "../../libs/img/favicons/manifest.json";
+import "../../libs/img/favicons/browserconfig.xml";
+
+// import all css files
+import "../../libs/css/normalize.css";
+import "../../libs/css/bootstrap.css";
+import "../../libs/css/owl.css";
+import "../../libs/css/animate.css";
 import "../../libs/fonts/font-awesome-4.1.0/css/font-awesome.min.css";
+import "../../libs/fonts/eleganticons/et-icons.css";
+import "../../libs/css/cardio.css";
 
 // import all js files
 import "../../libs/js/jquery-1.11.1.min.js";
@@ -18,33 +25,22 @@ import "../../libs/js/wow.min.js";
 import "../../libs/js/typewriter.js";
 import "../../libs/js/jquery.onepagenav.js";
 import "../../libs/js/templateMain.js";
-/*import "./template/tooltip.js";
-import "./template/typed.js";*/
-
-// import favicons
-import "../../libs/img/favicons/apple-touch-icon-60x60.png";
-import "../../libs/img/favicons/apple-touch-icon-57x57.png";
-import "../../libs/img/favicons/favicon-32x32.png";
-import "../../libs/img/favicons/favicon-16x16.png";
-
-// import other files
-import "../../libs/img/favicons/browserconfig.xml";
 
 // import React libraries
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-// import custom React components
+// import other components
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={Home}></IndexRoute>
-    </Route>
-  </Router>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+            <IndexRoute component={Home}></IndexRoute>
+        </Route>
+    </Router>,
 app);
