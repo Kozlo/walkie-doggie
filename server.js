2a2bba7b8c8acbd5d07fed839c5b70c15c89f823
Martins Kozlovskis <mkozlovskis@gmail.com>
2016-08-16 22:41:57 +0200

    Removed some passport-related code

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ const bodyParser = require('body-parser');
 const jwt = require('express-jwt');
 const cors = require('cors');
 
-//const session = require('express-session');
-//const http = require('http'); // TODO: check if this is needed
-//const passport = require('passport');
-//var LocalStrategy = require('passport-local').Strategy;
-
 // app
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
@@ -29,15 +24,6 @@ const mongoose = require('mongoose');
 
 // TODO: check what this is doing (Auth0-related)
 app.use(cors());
-// app general config
-// TODO: save the secret separately
-//app.use(session({
-//    secret: 'daddy needs new shoes',
-//    resave: false,
-//    saveUninitialized: true
-//}));
-//app.use(passport.initialize());
-//app.use(passport.session());
 // needed, otherwise body from post requests cannot be read
 app.use(bodyParser.text({
     type: 'text/plain'
@@ -52,13 +38,6 @@ const authCheck = jwt({
     secret: new Buffer('z4b9s5erPJsHkflIvoDDVnRkBUAG8fdXObt1Mix0eZElSvP3GAd0Wc36lsCth-tR', 'base64'),
     audience: 't48Z83ky2Kvi1NyuAA9j1Uop3pDi3b9n'
 });
-
-// passport config
-//const Account = require('./models/account');
-//passport.use(new LocalStrategy(Account.authenticate()));
-//passport.use(Account.createStrategy());
-//passport.serializeUser(Account.serializeUser());
-//passport.deserializeUser(Account.deserializeUser());
 
 // routes
 require('./routes/routes')(app);
